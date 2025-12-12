ec5007d5bb2dfef9cd23fcb90fa12e09015d268d
Akash <shridharshetty.a@northeastern.edu>
2025-12-12 15:12:08 -0500

    Remove admin page exports - auto-routed by Next.js

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -1,7 +1,8 @@
 /**
  * @file src/components/admin/index.ts
- * @description Export admin components for use in other files
+ * @description Admin components index
  */
-export { default as AdminDashboard } from "../../pages/admin/AdminDashboard";
-export { default as AdminUsers } from "../../pages/admin/adminUsers";
-export { default as AdminSkills } from "../../pages/admin/adminSkills";
+
+// Admin pages are auto-routed by Next.js from src/pages/admin/
+// No exports needed here
+export {};
